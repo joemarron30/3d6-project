a3d8f2559e9817d9a775eb245b25c2ca4adb944b
Joe Malachy Marron <marronjo@tcd.ie>
2021-02-08 18:22:04 +0000

    added more of the boiz

--- picture_practice/lib/main.dart
+++ picture_practice/lib/main.dart
@@ -11,22 +11,17 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('THE BOIZ'),
         ),
-        /*body: new Container(
-          color: Colors.purple[300],
-          child: new Image.asset('assets/larry.png'),
-          alignment: Alignment.center,
-        ),
-        new Container(
-          color: Colors.purple[400],
-          child: new Image.asset('assets/sassy.jfif'),
-          alignment: Alignment.center,
-        )*/
         body: ListView(
-          //padding: const EdgeInsets.all(10),
+          padding: const EdgeInsets.all(10),
           children: <Widget>[
             Container(
-              //title: Text('Larry Enticer'),
               child: new Image.asset('assets/larry.png'),
+            ),
+            Container(
+              child: new Image.asset('assets/sassy.jfif'),
+            ),
+            Container(
+              child: new Image.asset('assets/homer.png'),
             ),
           ]
         )
