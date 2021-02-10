e142721ef9ac1ad26e66cee9ee1b071c7f15956c
Joe Malachy Marron <marronjo@tcd.ie>
2021-02-10 13:01:36 +0000

    pages not working

--- picture_practice/lib/main.dart
+++ picture_practice/lib/main.dart
@@ -5,27 +5,52 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context){
-  	return MaterialApp(
-      title: 'THE BOIZ',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('THE BOIZ'),
-        ),
-        body: ListView(
-          padding: const EdgeInsets.all(10),
-          children: <Widget>[
-            Container(
-              child: new Image.asset('assets/larry.png'),
-            ),
-            Container(
-              child: new Image.asset('assets/sassy.jfif'),
-            ),
-            Container(
-              child: new Image.asset('assets/homer.png'),
-            ),
-          ]
-        )
-      ),
+    return Scaffold(
+      appBar: AppBar(title: Text('Pick a Country')),
+      body: Center(child: RaisedButton(
+        onPressed: () {
+          Navigator.of(context)
+            .push(
+              MaterialPageRoute(
+                builder: (context) => Page2()
+              )
+            );
+          },
+          child: Text('Canada'),
+      )),
     );
   }
 }
+
+class Page2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(title: Text('Larry Enticer')),
+      body: Center(child: RaisedButton(
+        onPressed: (){
+          Navigator.of(context)
+            .pop();
+        },
+        child: Text('Back to Countries')
+      )),
+    );
+  }
+}
+
+/*
+    body: ListView(
+     padding: const EdgeInsets.all(10),
+     children: <Widget>[
+       Container(
+         child: new Image.asset('assets/larry.png'),
+       ),
+       Container(
+         child: new Image.asset('assets/sassy.jfif'),
+       ),
+       Container(
+         child: new Image.asset('assets/homer.png'),
+       ),
+     ]
+   )
+ */
